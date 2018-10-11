b47ed6991f1ce5a396e79549edc8bfb55c20c640
Ya Yin <yayin@YadeMacBook-Pro.fritz.box>
2018-10-11 23:05:02 +0200

    insertion sort

--- InsertionSort.java
+++ InsertionSort.java
@@ -11,4 +11,18 @@ class InsertionSort {
             nums[j + 1] = key;
         }
     }
+
+    public void recursiveInsertionSort(int[] nums, int n) {
+        if (n <= 1)
+            return;
+        recursiveInsertionSort(nums, n - 1);
+        int last = nums[n - 1];
+        int j = n - 2;
+        while (j >= 0 && nums[j] > key) {
+            // move element forward
+            nums[j + 1] = nums[j];
+            j--;
+        }
+        nums[j + 1] = key;
+    }
 }
